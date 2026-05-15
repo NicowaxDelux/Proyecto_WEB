7100a6490785478ca7698321337d3f1a7e5edc28
NicowaxDelux <nycko111995@gmail.com>
2026-05-14 22:40:59 -0500

    condition upon logging in, Tailwind CSS desing was  added, add backbutton

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
+import BackButton from "../components/BackButton"
 
+export default function Login() {
 
-export default function login() {
-    
+    const router = useRouter();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -19,7 +22,14 @@ export default function login() {
         const data = await res.json();
 
         //guardar token
-        localStorage.setItem("token", data.token);
+        if(data.token){ 
+            localStorage.setItem("token", data.token);
+            
+            router.push("/");
+        } else {
+            alert("Error al logearte")
+        };
+
 
         alert("Login exitoso 🔐");
     };
@@ -27,22 +37,39 @@ export default function login() {
     return (
 
         <div>
-            <h1>Login</h1>
 
-            <input 
-                placeholder="Email"
-                onChange={e => setEmail(e.target.value)}
-            />
+            <BackButton />
 
-            <input  
-                type="password"
-                placeholder="Password"
-                onChange={e => setPassword(e.target.value)}
-            />
+            <div className="h-screen flex items-center justify-center bg-gray-100">
 
-            <button onClick={handleLogin}>
-                Iniciar sesión
-            </button>
+                <div className="bg-white p-8 rounded-2xl shadow-xl w-96">
+                <h1 className= "text-2x1 font-bold mb-6 text-center">
+                    Login 🔐
+                </h1>
+
+                <input 
+                    className="w-full mb-4 p-3 border rounded-x1"
+                    placeholder="Email"
+                    onChange={e => setEmail(e.target.value)}
+                />
+
+                <input  
+                    type="password"
+                    className="w-full mb-4 p-3 border rounded-1x"
+                    placeholder="Password"
+                    onChange={e => setPassword(e.target.value)}
+                />
+
+                <button 
+                    onClick={handleLogin}
+                    className="w-full bg-black text-white py-3 rounded-x1 hover:bg-yellow-600"
+                >
+
+                    Iniciar sesión
+                </button>
+
+                </div>
+            </div>
         </div>
     );
 }
