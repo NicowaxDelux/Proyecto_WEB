387344d000b51a612cf2c43f2198be7abe62efb5
NicowaxDelux <nycko111995@gmail.com>
2026-05-14 22:20:48 -0500

    Tailwind CSS desing was added in checkout

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Navbar from "../components/Navbar";
+import BackButton from "../components/BackButton";
 
 export default function Checkout() {
 
@@ -42,17 +44,34 @@ export default function Checkout() {
     };
 
     return (
-
+        
         <div>
-            <h1>Checkout</h1>
 
-            <button onClick={createOrder}>
-                Crear Orden
-            </button>
+            <Navbar />
+            
+            <div className="p-10 max-w-x1 mx-auto">
 
-            <button onClick={pay}>
-                Pagar
-            </button>
+                <BackButton />
+
+                <h1 className="text-3x1 font-bold mb-6">
+                    Checkout
+                </h1>
+
+                <div className="bg-white p-6 rounded-2x1 shadow">
+                    <button onClick={createOrder}
+                            className="w-full mb-4 bg-gray-200 py-3 rounded-xl"
+                    >
+                        Crear Orden
+                    </button>
+
+                    <button onClick={pay}
+                            className="w-full bg-black text-white py-3 rounded-x1 hover:bg-gray-800"
+                    >
+                        Pagar
+                    </button>
+                </div>
+
+            </div>
         </div>
     );
 }
