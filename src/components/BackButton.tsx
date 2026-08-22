c470c96713ea0a9ea0647eac843f3d12f57bed04
NicowaxDelux <nycko111995@gmail.com>
2026-08-21 21:36:17 -0500

    change path to /

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -8,7 +8,7 @@ export default function BackButton() {
     return (
 
         <button
-            onClick={ () => router.back()}
+            onClick={ () => router.push("/") }
             className= "fixed bottom-4 left-4 bg-black text-white px-4 py-2 rounded-xl shadow-lg"
         >
             Volver
